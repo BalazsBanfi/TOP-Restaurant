8e5e5548311ac8aa44e3a9f25dd2895fa5834699
BalazsBanfi <banfibalazs81@gmail.com>
2023-06-27 14:01:19 +0200

    add localization info

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-import { renderHeader } from './header.js';
-import { renderNav } from './nav.js';
-import { tabMenu, tabLocal, tabContact } from './main.js';
-import { renderFooter } from './footer.js';
-import './style.css';
+import { renderHeader } from "./header.js";
+import { renderNav } from "./nav.js";
+import { tabMenu, tabLocal, tabContact } from "./main.js";
+import { renderFooter } from "./footer.js";
+import "./style.css";
 
-
-const content = document.getElementById('content');
+const content = document.getElementById("content");
 
 renderHeader(content);
 renderNav(content);
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import menuImg from './pizzaMenu.png';
+import localImg from './local.png';
+
 export function tabMenu(content) {
     main.innerHTML = "";
     const menuPage = new Image();
@@ -7,9 +9,31 @@ export function tabMenu(content) {
 }
 
 export function tabContact() {
-  main.innerHTML = "Contact";
+  const contacts = [
+    ["Evy's glutenfree pizza", 'Call me for further information on +36 70 123-45678'],
+    ['Bally, manager', 'Technical informations: +36 70 223-4455'],
+    ['Nori, the cooking assistant', 'Call for table reservation: +36 70 444-5432'],
+    ['Bazsi, services', 'Call for repair the car +36 70 888-7654']
+  ]
+  main.innerHTML = "";
+
+  
+
+  const divMain = document.createElement('div');
+  divMain.setAttribute('class', 'divMain');
+  const divMainH1 = document.createElement('h1');
+  divMainH1.textContent = "Evy's glutenfree pizzas";
+  divMain.appendChild(divMainH1);
+  const divMainP = document.createElement('p');
+  divMainP.textContent = "Call me for further information on +36 70 123-45678";
+  divMain.appendChild(divMainP);
+  main.appendChild(divMain);
 }
 
+
 export function tabLocal() {
-  main.innerHTML = "Localization";
+  main.innerHTML = "";
+  const local = new Image();
+  local.src = localImg;
+  main.appendChild(local);
 }
